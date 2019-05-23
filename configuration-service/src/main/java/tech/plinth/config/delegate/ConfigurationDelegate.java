afb05be8c75fc85d074da6fb0d54a3e2b1ae9231
João Flora <joao.flora@mindera.com>
2019-05-23 14:09:28 +0100

    Get last version of configuration
    create model Base
    
    endpoint implemented
    
    create test structure
    
    adding more tests, in process

--- configuration-service/src/main/java/tech/plinth/config/delegate/ConfigurationDelegate.java
+++ configuration-service/src/main/java/tech/plinth/config/delegate/ConfigurationDelegate.java
@@ -1,11 +1,16 @@
 package tech.plinth.config.delegate;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.github.fge.jsonpatch.JsonPatchException;
+import com.github.fge.jsonpatch.mergepatch.JsonMergePatch;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
 import tech.plinth.config.database.model.Configuration;
+import tech.plinth.config.database.repository.BaseRepository;
 import tech.plinth.config.database.repository.ConfigurationRepository;
 import tech.plinth.config.interceptor.model.RequestContext;
 
@@ -18,6 +23,9 @@ public class ConfigurationDelegate {
 
     @Autowired
     private ConfigurationRepository configurationRepository;
+
+    @Autowired
+    private BaseRepository baseRepository;
 
     @Resource
     private RequestContext requestContext;
@@ -37,6 +45,7 @@ public class ConfigurationDelegate {
     }
 
     /**
+     * ®
      * define and return the next version number to be created
      * find the max version number in DB and the next version will be the that version number + 1
      */
@@ -48,4 +57,27 @@ public class ConfigurationDelegate {
                 .getVersion() + 1L;
     }
 
+    /**
+     * return last version merged with base configuration from that platform
+     */
+    public JsonNode getLastVersion() throws JsonPatchException {
+
+        JsonNode jsonVersionData = configurationRepository.findTopByPlatformOrderByVersionDesc(requestContext.getPlatformId())
+                .orElseThrow(() -> {
+                    logger.error("Platform:{} RequestId:{} Message: No configuration defined to this platform",
+                            requestContext.getPlatformId(), requestContext.getRequestId());
+                    throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No configuration defined to this platform");
+                }).getDataJson();
+
+        JsonNode jsonBaseData = baseRepository.findTopByOrderByVersionDesc().orElseThrow(() -> {
+            logger.error("Platform:{} RequestId:{} Message: No Base configuration found");
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No Base configuration found");
+        }).getDataJson();
+
+        JsonMergePatch patch = JsonMergePatch.fromJson(jsonVersionData);
+
+        JsonNode jsonMerged = patch.apply(jsonBaseData);
+
+        return jsonMerged;
+    }
 }
